cad7cf4d40b52ce3ace0726da2607e26c9eb8b13
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2026-02-07 18:55:51 +0600

    product-api

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth-guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Role } from '@prisma/client';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { CreateProductDto } from './dto/create-product-.dto';
+import { ProductResponseDto } from './dto/product-response.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Role(Role.ADMIN)
+  @ApiBearerAuth('JWT-auth')
+  @ApiOperation({ summary: 'Create a new product ' })
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+    type: ProductResponseDto,
+  })
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Sku already exist',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully ',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden admin role required',
+  })
+  async create(
+    @Body() CreateProductDto: CreateProductDto,
+  ): Promise<ProductResponseDto> {
+    return await this.productService.create(CreateProductDto);
+  }
+}
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product-.dto';
+import { ProductResponseDto } from './dto/product-response.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+  // Create product
+  async create(
+    createProductDto: CreateProductDto,
+  ): Promise<ProductResponseDto> {
+    const existingSku = await this.prisma.product.findUnique({
+      where: { sku: createProductDto.sku },
+    });
+    if (existingSku) {
+      throw new ConflictException(
+        `Product with SKU ${createProductDto.sku} already exist`,
+      );
+    }
+
+    const product = await this.prisma.product.create({
+      data: {
+        ...createProductDto,
+        price: new Prisma.Decimal(createProductDto.price),
+      },
+      include: {
+        category: true,
+      },
+    });
+
+    return this.formatProduct(product);
+  }
+}
